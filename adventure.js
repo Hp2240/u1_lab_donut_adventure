5d24743486169c6a0093b258f45c5da1aeafe016
Hyeree Park <hp2240@nyu.edu>
2022-06-30 15:35:21 -0400

    created hamster class

---
+++ adventure.js
@@ -0,0 +1,16 @@
+class Hamster {
+  constructor(owner, name, price) {
+    this.owner = owner
+    this.name = name
+    this.price = price
+  }
+  whieelReun() {
+    console.log('squeak squeak')
+  }
+  eatFood() {
+    console.log('nibble nibble')
+  }
+  getPrice() {
+    return price
+  }
+}
